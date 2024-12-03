af49dd8fb70fa99b7d16426a07c98d1b7bcb76e0
ordinarydennis <ordinarydennis@gmail.com>
2024-12-04 08:07:14 +0900

    Top Interview 150 Array / String - 80. Remove Duplicates from Sorted Array II(read editorial)

--- Array-String/80_Remove_Duplicates_from_Sorted_Array_II.h
+++ Array-String/80_Remove_Duplicates_from_Sorted_Array_II.h
@@ -104,3 +104,31 @@ public:
 
 	}
 }
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+
+		int index = 1;
+		int count = 1;
+
+		for (int i = 1; i < nums.size(); i++)
+		{
+			if (nums[i] == nums[i - 1])
+			{
+				count++;
+			}
+			else
+			{
+				count = 1;
+			}
+
+			if (count <= 2)
+			{
+				nums[index++] = nums[i];
+			}
+		}
+
+		return index;
+	}
+};
