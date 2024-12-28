a5aebd4825aef4f9d2ee5928727e508479ea120c
ordinarydennis <ordinarydennis@gmail.com>
2024-12-28 23:32:27 +0900

    Top Interview 150 Matrix - 54. Spiral Matrix(read editorial)

--- Matrix/54_Spiral_Matrix.h
+++ Matrix/54_Spiral_Matrix.h
@@ -43,3 +43,54 @@ public:
 		return ret;
 	}
 };
+
+
+
+class Solution {
+public:
+	vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+		int maxX = matrix[0].size() - 1;
+		int maxY = matrix.size() - 1;
+		int minX = 0;
+		int minY = 0;
+
+		vector<int> ret;
+
+		while (ret.size() < matrix.size() * matrix[0].size())
+		{
+			for (int x = minX; x <= maxX; x++)
+			{
+				ret.push_back(matrix[minY][x]);
+			}
+
+			for (int y = minY + 1; y <= maxY; y++)
+			{
+				ret.push_back(matrix[y][maxX]);
+			}
+
+			if (minY != maxY)
+			{
+				for (int x = maxX - 1; minX <= x; x--)
+				{
+					ret.push_back(matrix[maxY][x]);
+				}
+			}
+			if (minX != maxX)
+			{
+				for (int y = maxY - 1; minY < y; y--)
+				{
+					ret.push_back(matrix[y][minX]);
+				}
+			}
+
+			minX++;
+			minY++;
+			maxX--;
+			maxY--;
+		}
+
+		return ret;
+	}
+};
+
