1b6fa350cab685cf1637b9c622dd7076109a68a2
ordinarydennis <ordinarydennis@gmail.com>
2024-12-30 19:41:55 +0900

    Top Interview 150 Matrix - 48. Rotate Image(read editorial)

--- Matrix/48_Rotate_Image.h
+++ Matrix/48_Rotate_Image.h
@@ -28,3 +28,52 @@ public:
 
 	}
 };
+
+class Solution {
+public:
+	void rotate(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+
+		for (int i = 0; i < (n + 1) / 2; i++)
+		{
+			for (int j = 0; j < n / 2; j++)
+			{
+				int temp = matrix[n - 1 - j][i];
+
+				matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+
+				matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+
+				matrix[j][n - 1 - i] = matrix[i][j];
+
+				matrix[i][j] = temp;
+			}
+		}
+	}
+};
+
+class Solution {
+public:
+	void rotate(vector<vector<int>>& matrix) {
+		transpose(matrix);
+		reflect(matrix);
+	}
+
+private:
+	void transpose(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				swap(matrix[j][i], matrix[i][j]);
+			}
+		}
+	}
+	void reflect(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n / 2; j++) {
+				swap(matrix[i][j], matrix[i][n - j - 1]);
+			}
+		}
+	}
+};
