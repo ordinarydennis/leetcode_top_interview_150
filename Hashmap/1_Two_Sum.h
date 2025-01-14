6884d5d1794bb09cade64c02908d8ebb5337eee6
ordinarydennis <ordinarydennis@gmail.com>
2025-01-14 14:02:48 +0900

    Top Interview 150 Hashmap - 1. Two Sum(read editorial)

--- Hashmap/1_Two_Sum.h
+++ Hashmap/1_Two_Sum.h
@@ -36,3 +36,68 @@ public:
 		return {};
 	}
 };
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> hash;
+		for (int i = 0; i < nums.size(); i++) {
+			hash[nums[i]] = i;
+		}
+		for (int i = 0; i < nums.size(); i++) {
+			int complement = target - nums[i];
+			if (hash.find(complement) != hash.end() && hash[complement] != i) {
+				return { i, hash[complement] };
+			}
+		}
+		// If no valid pair is found, return an empty vector
+		return {};
+	}
+};
+
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+
+		unordered_map<int, int> m;
+
+		for (int i = 0; i < nums.size(); i++)
+			m[nums[i]] = i;
+
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			auto it = m.find(target - nums[i]);
+			if (m.end() == it || it->second == i)
+				continue;
+			
+			return vector<int> { i, it->second };
+		}
+
+		return {};
+	}
+};
+
+//one way
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+
+		unordered_map<int, int> m;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			auto it = m.find(target - nums[i]);
+			if (m.end() == it)
+			{
+				m[nums[i]] = i;
+				continue;
+			}
+
+			return { i, it->second };	
+		}
+
+		return {};
+	}
+};
