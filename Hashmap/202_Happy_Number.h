0bc0882b21af1453494efa3bb97e4c52cd65caa7
ordinarydennis <ordinarydennis@gmail.com>
2025-01-16 22:38:23 +0900

    Top Interview 150 Hashmap - 202. Happy Number(read editorial)

--- Hashmap/202_Happy_Number.h
+++ Hashmap/202_Happy_Number.h
@@ -27,3 +27,32 @@ public:
 		return 1 == num;
 	}
 };
+
+class Solution {
+public:
+	bool isHappy(int n) {
+
+		int num = n;
+
+		unordered_set<int> set;
+
+		while (1 != num && 0 == set.count(num))
+		{
+			set.insert(num);
+			
+			int sum = 0;
+
+			while (0 < num)
+			{
+				int m = num % 10;
+				sum += m * m;
+				num = num / 10;
+			}
+
+			num = sum;
+		}
+
+		return 1 == num;
+
+	}
+};
