4a6a0d1dbb07c4bc4f4e5e1ecdb5a20bd4e1d44a
ordinarydennis <ordinarydennis@gmail.com>
2025-01-21 21:16:20 +0900

    Top Interview 150 Intervals - 57. Insert Interval(read editorial)

--- Intervals/57_Insert_Interval.h
+++ Intervals/57_Insert_Interval.h
@@ -4,63 +4,71 @@ public:
 
 		vector<vector<int>> ret;
 
-		if (intervals.empty())
+		int len = intervals.size();
+
+		int i = 0;
+
+		while (i < len && intervals[i][1] < newInterval[0])
 		{
-			ret.emplace_back(std::move(newInterval));
-			return ret;
+			ret.push_back(intervals[i++]);
 		}
 
-		vector<int> overlapped;
-
-		bool isInserted = false;
-
-		for (int i = 0; i < intervals.size(); i++)
+		while (i < len && intervals[i][0] <= newInterval[1])
 		{
-			auto& interval = intervals[i];
-
-			if (overlapped.empty())
-			{
-				if (interval[0] <= newInterval[0] && newInterval[1] <= interval[1])
-				{
-					isInserted = true;
-					ret.emplace_back(std::move(interval));
-					continue;
-				}
-
-				if (interval[1] < newInterval[0])
-				{
-					ret.emplace_back(std::move(interval));
-					continue;
-				}
-
-				overlapped = vector<int> {
-					std::min(interval[0], newInterval[0]),
-					std::max(interval[1], newInterval[1])
-				};
-			}
-			else
-			{
-				if (interval[0] <= overlapped[1])
-				{
-					overlapped[1] = std::max(interval[1], newInterval[1]);
-				}
-				else
-				{
-					ret.emplace_back(std::move(overlapped));
-					ret.emplace_back(std::move(interval));
-					isInserted = true;
-				}
-			}
+			newInterval[0] = std::min(newInterval[0], intervals[i][0]);
+			newInterval[1] = std::max(newInterval[1], intervals[i][1]);
+			i++;
 		}
 
-		if (false == isInserted)
+		ret.push_back(newInterval);
+
+		while (i < len)
 		{
-			ret.emplace_back(std::move(newInterval));
+			ret.push_back(intervals[i++]);
 		}
 
-		if (false == overlapped.empty())
+		return ret;
+	}
+};
+
+
+class Solution {
+public:
+	vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+		vector<vector<int>> ret;
+
+		int len = intervals.size();
+
+		int i = 0;
+
+		while (i < len && intervals[i][1] < newInterval[0])
 		{
-			ret.emplace_back(std::move(overlapped));
+			ret.push_back(intervals[i++]);
+		}
+
+		//intervals[i][1] >= newInterval[0]
+
+		//이렇게 하면 연결되지 않은 다음 interval이 조건에 걸리게 된다. 
+		//while (i < len && intervals[i][1] >= newInterval[0])
+		
+		//intervals[i][0] <= newInterval[1] 조건은 아래 상황 처럼 인터벌을 빠져나가는 종료 조건이다.
+		//  ___              ___
+		// |   |      ->    |   |
+		//    |___|               |___|
+
+		while (i < len && intervals[i][0] <= newInterval[1])
+		{
+			newInterval[0] = std::min(newInterval[0], intervals[i][0]);
+			newInterval[1] = std::max(newInterval[1], intervals[i][1]);
+			i++;
+		}
+
+		ret.push_back(newInterval);
+
+		while (i < len)
+		{
+			ret.push_back(intervals[i++]);
 		}
 
 		return ret;
