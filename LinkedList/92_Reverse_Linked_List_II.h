88eae3b6a9d38752ef847c90c080435a4eb215c6
ordinarydennis <ordinarydennis@gmail.com>
2025-01-31 22:06:58 +0900

    Top Interview 150 Linked List - 92. Reverse Linked List II(try again, fail)

--- LinkedList/92_Reverse_Linked_List_II.h
+++ LinkedList/92_Reverse_Linked_List_II.h
@@ -8,6 +8,86 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+		ListNode* node = head;
+		ListNode* leftNode = nullptr;
+		ListNode* rightNode = nullptr;
+		ListNode* pre = nullptr;
+		ListNode* next = nullptr;
+
+		int index = 1;
+
+		while (node && index <= right + 1)
+		{
+			if (left - 1 == index)
+			{
+				pre = node;
+			}
+			else if (right + 1 == index)
+			{
+				next = node;
+			}
+			else if (left == index)
+			{
+				leftNode = node;
+			}
+			else if (right == index)
+			{
+				rightNode = node;
+			}
+
+			node = node->next;
+
+			index++;
+		}
+
+		bool isLast = (nullptr != rightNode && nullptr == rightNode->next);
+		if (leftNode && rightNode)
+		{
+			node = leftNode;
+
+			ListNode* pre2 = nullptr;
+			int i = right - left + 1;
+			while (i)
+			{
+				auto* temp = node->next;
+				node->next = pre2;
+				pre2 = node;
+				node = temp;
+				i--;
+			}
+
+			if (pre)
+			{
+				pre->next = rightNode;
+			}
+			
+			leftNode->next = next;
+		}
+
+		if (isLast)
+		{
+			return rightNode;
+		}
+
+		return head;
+	}
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 
 class Solution {
 public:
