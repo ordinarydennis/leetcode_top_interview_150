e4f7e5831c4b336519cffb41c4f75289545d89da
ordinarydennis <ordinarydennis@gmail.com>
2025-02-03 14:09:48 +0900

    Top Interview 150 Linked List - 82. Remove Duplicates from Sorted List II(fail)

---
+++ LinkedList/82_Remove_Duplicates_from_Sorted_List_II.h
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+
+		ListNode* node = head;
+		ListNode* pre = head;
+
+		while (node)
+		{
+			if (nullptr == pre && node->next && node->val != node->next->val)
+			{
+				pre = node;
+			}
+			else if(pre && node->next && node->val != node->next->val)
+			{
+				pre->next = node->next;
+				pre = nullptr;
+			}
+			
+			node = node->next;
+		}
+
+		return head;
+	}
+};
+
