005b2398b46e8ec6a73e7a967c137ad136cdb251
ordinarydennis <ordinarydennis@gmail.com>
2025-02-09 21:53:40 +0900

    Top Interview 150 Linked List - 82. Remove Duplicates from Sorted List II(read editorial)

--- LinkedList/82_Remove_Duplicates_from_Sorted_List_II.h
+++ LinkedList/82_Remove_Duplicates_from_Sorted_List_II.h
@@ -1,3 +1,80 @@
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+
+		ListNode* ret = new ListNode(0, head);
+		ListNode* pre = ret;
+		ListNode* node = head;
+
+		while (node)
+		{
+			if (node->next && node->val == node->next->val)
+			{
+				while (node->next && node->val == node->next->val)
+				{
+					node = node->next;
+				}
+
+				pre->next = node->next;
+			}
+			else
+			{
+				pre = node;
+			}
+
+			node = node->next;
+
+		}
+
+		return ret->next;
+	}
+};
+
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		// sentinel
+		ListNode* sentinel = new ListNode(0, head);
+		// predecessor = the last node
+		// before the sublist of duplicates
+		ListNode* pred = sentinel;
+		while (head != NULL) {
+			// If it's a beginning of duplicates sublist
+			// skip all duplicates
+			if (head->next != NULL && head->val == head->next->val) {
+				// Move till the end of duplicates sublist
+				while (head->next != NULL && head->val == head->next->val) {
+					head = head->next;
+				}
+				// Skip all duplicates
+				pred->next = head->next;
+				// otherwise, move predecessor
+			}
+			else {
+				pred = pred->next;
+			}
+			// move forward
+			head = head->next;
+		}
+		return sentinel->next;
+	}
+};
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
