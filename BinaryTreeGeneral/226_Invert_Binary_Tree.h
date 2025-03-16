a58464f0e8f22c7474e4713d712de6446961ad4f
ordinarydennis <ordinarydennis@gmail.com>
2025-03-16 18:48:28 +0900

    Top Interview 150 Binary Tree General - 226. Invert Binary Tree(10m)

--- BinaryTreeGeneral/226_Invert_Binary_Tree.h
+++ BinaryTreeGeneral/226_Invert_Binary_Tree.h
@@ -56,3 +56,40 @@ public:
 		return root;
 	}
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+	void dfs(TreeNode* node)
+	{
+		if (!node)
+			return;
+
+		std::swap(node->left, node->right);
+
+		if (node->left)
+			dfs(node->left);
+
+		if (node->right)
+			dfs(node->right);
+	}
+
+
+public:
+	TreeNode* invertTree(TreeNode* root) {
+
+		dfs(root);
+
+		return root;
+	}
+};
